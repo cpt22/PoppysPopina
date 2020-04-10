38bb077d782a93cf249a74cc07decf5ce2e9fafd
Christian <ctingle18@gmail.com>
2020-04-10 11:00:20 -0500

    fixed

---
+++ html/404.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+<?php require_once SRC . 'components/header.php'; ?>
+<title>You Lend To Me</title>
+</head>
+<body>
+	<?php require_once SRC . 'components/navbar.php'; ?>
+	
+	PAGE NOT FOUND
+	
+	<?php require_once SRC . 'components/footer.php'; ?>
+</body>
+</html>
